eecbd5f89bc0e5ca59f2ac64c045c2e0b1b2b559
vi2hnu <vishnuvaradhankr@gmail.com>
2026-01-02 22:21:31 +0530

    Update: changes in naming convention

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'auth', loadChildren: () => import('./modules/auth/auth-module').then(m => m.AuthModule)},
+    {path: 'auth', loadChildren: () => import('./features/auth/auth-module').then(m => m.AuthModule)},
 ];
