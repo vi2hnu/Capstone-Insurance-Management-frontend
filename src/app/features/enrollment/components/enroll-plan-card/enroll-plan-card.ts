8442847682db93ff3c5ba6a013d60b7ebf802060
vi2hnu <vishnuvaradhankr@gmail.com>
2026-01-05 09:18:06 +0530

    Added: mock payment gateway

--- src/app/features/enrollment/components/enroll-plan-card/enroll-plan-card.ts
+++ src/app/features/enrollment/components/enroll-plan-card/enroll-plan-card.ts
@@ -1,37 +1,45 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, NgZone } from '@angular/core';
 import { PlanModel } from '../../../../core/model/policy/plolicy-model';
 import { EnrollmentService } from '../../service/enrollment-service';
 import { Roleservice } from '../../../../core/service/roleservice';
 import { UserService } from '../../../../core/service/user/user-service';
+import { Billing } from '../../service/billing';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-enroll-plan-card',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './enroll-plan-card.html',
   styleUrl: './enroll-plan-card.css',
 })
 export class EnrollPlanCard {
   @Input() plan!: PlanModel;
   @Input() userId!: string;
-
+  
   private enrollmentService = inject(EnrollmentService);
   private roleService = inject(Roleservice);
   private userService = inject(UserService);
-
+  private billingService = inject(Billing);
+  private ngZone = inject(NgZone);
+  
   successMessage = '';
   errorMessage = '';
+  isProcessing = false; 
 
   enroll(): void {
+    this.errorMessage = '';
+    this.successMessage = '';
+    
     if (!this.userId) {
       this.errorMessage = 'Invalid user';
       return;
     }
 
     const role = this.roleService.getRole();
-
     const request: any = {
       userId: this.userId,
-      planId: this.plan.id,
+      planId: this.plan.id
     };
 
     if (role === 'INSURANCE_AGENT') {
@@ -41,20 +49,95 @@ export class EnrollPlanCard {
         return;
       }
       request.agentId = agentId;
+      this.enrollPolicy(request);
+      return;
     }
 
-    this.enrollmentService.enrollPlan(request)
+    this.isProcessing = true;
+    this.billingService
+      .createOrder(this.userId, this.plan.premiumAmount)
       .subscribe({
-        next: (policy) => {
-          if (policy) {
-            this.successMessage = `Successfully enrolled in ${this.plan.name}!`;
-            this.errorMessage = '';
-          }
-        },
+        next: (response) => this.openTransactionModal(response),
         error: () => {
-          this.errorMessage = 'Failed to enroll. Please try again.';
+          this.isProcessing = false;
+          this.errorMessage = 'Failed to create payment order';
         }
       });
+  }
 
+  openTransactionModal(response: any) {
+    const options = {
+      order_id: response.orderId,
+      key: "rzp_test_RyB1uzTAASmWLT",
+      amount: response.amount,
+      currency: response.currency,
+      name: 'Smart health insurance company',
+      description: 'Enroll under plan',
+      image: '',
+      handler: (resp: any) => {
+        this.ngZone.run(() => {
+          this.processResponse(resp);
+        });
+      },
+      modal: {
+        ondismiss: () => {
+          this.ngZone.run(() => {
+            this.isProcessing = false;
+            this.errorMessage = 'Payment cancelled';
+          });
+        }
+      },
+      prefill: {
+        name: "insurance",
+        email: "contact@gmail.com",
+        contact: '9999999'
+      },
+      notes: {
+        address: "insurance company"
+      },
+      theme: {
+        color: '#3399cc'
+      }
+    };
+
+    const razorpay = new (window as any).Razorpay(options);
+    razorpay.open();
+  }
+
+  processResponse(resp: any) {
+    const paymentVerificationRequest = {
+      razorpayOrderId: resp.razorpay_order_id,
+      razorpayPaymentId: resp.razorpay_payment_id,
+      razorpaySignature: resp.razorpay_signature
+    };
+
+    this.billingService.verifyPayment(paymentVerificationRequest).subscribe({
+      next: () => {
+        this.enrollPolicy({
+          userId: this.userId,
+          planId: this.plan.id
+        });
+      },
+      error: (err) => {
+        console.error('Payment verification failed:', err);
+        this.isProcessing = false;
+        this.errorMessage = 'Payment verification failed';
+      }
+    });
+  }
+
+  private enrollPolicy(request: any) {
+    this.enrollmentService.enrollPlan(request).subscribe({
+      next: () => {
+        this.isProcessing = false;
+        this.successMessage = 'Enrollment success';
+        console.log('Enrollment successful!');
+      },
+      error: (err) => {
+        console.error('Enrollment failed:', err);
+        this.isProcessing = false;
+        this.errorMessage = 'Failed to enroll. Please try again.';
+      }
+    });
   }
 }
---
+++ src/app/features/enrollment/service/billing.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Billing {
+  private http = inject(HttpClient);
+  private baseUrl = 'http://localhost:9000/billingservice/api/payment';
+
+  createOrder(userId: string, amount: number): Observable<any> {
+    const request = {userId,amount,purpose: 'POLICY_ENROLLMENT'};
+
+    return this.http.post<any>(`${this.baseUrl}/create/order`, request);
+  }
+
+  verifyPayment(request: {razorpayOrderId: string; razorpayPaymentId: string; razorpaySignature: string;}) {
+  return this.http.post(`${this.baseUrl}/verify/order`,request);
+}
+}
